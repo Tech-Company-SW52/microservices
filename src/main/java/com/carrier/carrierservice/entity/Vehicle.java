fb7340be6382a739991bc96e04a6cb4436191fe9
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2023-09-28 13:11:16 -0500

    feat(vehicle): entity, repository & service

---
+++ src/main/java/com/carrier/carrierservice/entity/Vehicle.java
@@ -0,0 +1,34 @@
+package com.carrier.carrierservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "vehicles")
+public class Vehicle implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carrier_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Carrier carrier;
+
+    @Column(name = "photo", nullable = false)
+    private String photo;
+
+    @NotEmpty(message = "El tipo no debe ser vacío")
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @NotEmpty(message = "La cantidad no debe ser vacío")
+    @Column(name = "quantity", nullable = false)
+    private Long quantity;
+}
