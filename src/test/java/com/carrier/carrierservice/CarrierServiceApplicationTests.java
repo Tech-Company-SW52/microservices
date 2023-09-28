95150805e1bcf0a1f96a80432d60fe4b037c3e3b
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2023-09-28 13:09:17 -0500

    feat(carrier): entity, repository & service

---
+++ src/test/java/com/carrier/carrierservice/CarrierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrier.carrierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
