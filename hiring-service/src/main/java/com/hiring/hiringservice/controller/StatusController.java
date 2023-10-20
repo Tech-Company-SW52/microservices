ef4fd4229d9991e92c3d97500a8900d602d95505
JennyBalceda <89040419+JennyBalceda@users.noreply.github.com>
2023-10-20 17:32:40 -0500

    Update StatusController.java

--- hiring-service/src/main/java/com/hiring/hiringservice/controller/StatusController.java
+++ hiring-service/src/main/java/com/hiring/hiringservice/controller/StatusController.java
@@ -14,32 +14,8 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/statusContract")
+@RequestMapping("/")
 @Api(tags="Status Contract", value="Web Service RESTful of Status Contracts")
 public class StatusController {
-    private final StatusService statusContractService;
-
-    public StatusController(StatusService statusContractService) {
-        this.statusContractService = statusContractService;
-    }
-
-    //Retornar todos los status
-    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value="List of Status Contract", notes="Method to list all status contracts")
-    @ApiResponses({
-            @ApiResponse(code=201, message="Status contracts found"),
-            @ApiResponse(code=404, message="Status contracts not found"),
-            @ApiResponse(code=501, message="Internal server error")
-    })
-    public ResponseEntity<List<StatusContract>> findAllStatus() {
-        try {
-            List<StatusContract> statusContracts = statusContractService.getAll();
-            if (statusContracts.size() > 0)
-                return new ResponseEntity<>(statusContracts, HttpStatus.OK);
-            else
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception ex) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    
 }
